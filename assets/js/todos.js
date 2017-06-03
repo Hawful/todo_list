854a7214b4a3d02a8dbd45e71e5f2024d2da48fa
Michael Hoff <hoffie.hoff@gmail.com>
2017-06-03 01:17:38 -0700

    Finished basic functionality. Adding and deleting todos.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,9 +1,25 @@
 //Check off todos by clicking
 
-$("li").on("click", function(){
+//When using .on() recognize that it can only be applied to new elements
+// if the elements are children of a selected parent element.
+
+//For instance, this statement shows an example of a ul element as the parent
+// who then turns and gives a function to every li under her.
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed")
 });
 
-$("span").on("click", function(){
-	$(this).closest("li").remove();
-})
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X </span>" + todoText + "</li>");
+	}
+});
